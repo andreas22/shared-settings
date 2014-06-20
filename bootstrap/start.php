c5dae3c0964b7707ea0dd89d6a5dc50efb3845c9
Andreas Christodoulou <andreas22@gmail.com>
2014-06-21 01:05:41 +0300

    Change env detection, if .env_local.php exists the it will return the env. Change default database in production to sqlite, Change debug default value for production to false

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,14 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('homestead'),
-
-));
+$env = $app->detectEnvironment(function () {
+    $filename = __DIR__ . '/../.env_local.php';
+    if (file_exists($filename)) {
+        return include($filename);
+    } else {
+        return 'production';
+    }
+});
 
 /*
 |--------------------------------------------------------------------------
